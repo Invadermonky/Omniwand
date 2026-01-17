574172e2ced0dad698b6a94b0efb661dcb47e6e1
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-01-17 00:45:55 -0800

    fix item frame dupe

--- src/main/java/com/invadermonky/omniwand/handlers/CommonEventHandler.java
+++ src/main/java/com/invadermonky/omniwand/handlers/CommonEventHandler.java
@@ -4,10 +4,14 @@ import com.invadermonky.omniwand.Omniwand;
 import com.invadermonky.omniwand.registry.Registry;
 import com.invadermonky.omniwand.util.WandHelper;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.item.EntityItemFrame;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -16,9 +20,8 @@ public class CommonEventHandler {
     @SubscribeEvent
     public static void onItemBroken(PlayerDestroyItemEvent event) {
         EntityPlayer player = event.getEntityPlayer();
-        if(WandHelper.isOmniwand(event.getOriginal())) {
-            ItemStack wandStack = WandHelper.removeItemFromWand(event.getOriginal(), true, stack -> {
-            });
+        if(WandHelper.isTransformedWand(event.getOriginal())) {
+            ItemStack wandStack = WandHelper.removeItemFromWand(event.getOriginal(), true, stack -> {});
             if (player != null && !player.addItemStackToInventory(wandStack)) {
                 player.dropItem(wandStack, true);
             }
@@ -35,6 +38,22 @@ public class CommonEventHandler {
                 ItemStack wandStack = WandHelper.removeItemFromWand(stack, false, entityItem::setItem);
                 player.inventory.setInventorySlotContents(player.inventory.currentItem, wandStack);
             }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEntityInteract(PlayerInteractEvent.EntityInteract event) {
+        EntityPlayer player = event.getEntityPlayer();
+        ItemStack stack = player.getHeldItem(event.getHand());
+        World world = player.world;
+        if(event.getTarget() instanceof EntityItemFrame && WandHelper.isOmniwand(stack) && !player.isCreative()) {
+            EntityItemFrame itemFrame = (EntityItemFrame) event.getTarget();
+            if(!world.isRemote && itemFrame.getDisplayedItem().isEmpty()) {
+                itemFrame.setDisplayedItem(stack);
+                stack.shrink(1);
+            }
+            event.setCancellationResult(EnumActionResult.SUCCESS);
+            event.setCanceled(true);
         }
     }
 }
